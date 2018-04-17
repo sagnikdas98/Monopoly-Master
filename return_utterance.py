b56892ea2b21430fbf0cd138d941c9704de9c76a
sagnikdas98 <sagnikdas98@hotmail.com>
2018-04-17 20:50:03 +0530

    12 warnings

--- return_utterance.py
+++ return_utterance.py
@@ -1,1 +1,6 @@
-ret_launch=["Welcome to Monopoly Master! How many players do we have today"]
+import random
+ret_launch=["Welcome to Monopoly Master","Hey there I'm monoploy master","Hello. You have summoned the monopoly master"]
+
+def random_statement(phraselist):
+    statement=phraselist[random.randrange(len(phraselist))]
+    return statement
