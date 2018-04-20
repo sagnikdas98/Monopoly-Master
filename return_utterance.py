4daf78cf83477c7cbe6748b3048e66d59417eae8
sagnikdas98 <sagnikdas98@hotmail.com>
2018-04-20 16:40:29 +0530

    board object

--- return_utterance.py
+++ return_utterance.py
@@ -1,20 +1,20 @@
 import random
-ret_launch=["Welcome to Monopoly Master","Hey there I'm monopoly master","Hello. You have summoned the monopoly master"]
+ret_launch=["Welcome to Monopoly Master. ","Hey there I'm monopoly master. ","Hello. You have summoned the monopoly master. "]
 
-ask_no_players=["How many players do we have today","How many are in","How many of you are playing today"]
+ask_no_players=["How many players do we have today. ","How many are in. ","How many of you are playing today. "]
 
-confirmation=["Roger that","okay, got it"]
+confirmation=["Roger that. ","okay, got it. "]
 
-too_many=["Oops, I cant handle more than four players.", "That's a lot of players for me to handle.",
-          "I can only manage two to four players.", "I m sorry. I can only manage two to four players."]
+too_many=["Oops, I cant handle more than four players. ", "That's a lot of players for me to handle. ",
+          "I can only manage two to four players. ", "I m sorry. I can only manage two to four players. "]
 
-ask_again=["so how many players now?","please consider your players again",]
+ask_again=["so how many players now? ","please consider your players again. ",]
 
-not_valid=["The answer didn't satisfy my question.", "The wasn't the answer i was looking for"]
+not_valid=["That answer didn't satisfy my question. ", "The wasn't the answer I was looking for. "]
 
-alone=["oops, you cannot play this game alone","You look pretty lonely"]
+alone=["Oops, you cannot play this game alone. ","You look pretty lonely. "]
 
-setting_board=["Setting up the board.","Creating your game.","Getting your game ready"]
+setting_board=["Setting up the board. ","Creating your game. ","Getting your game ready. "]
 
 win=[]
 
@@ -24,7 +24,7 @@ def format_statement(phrase,formatlist):
     return phrase.format(formatlist)
 
 def combine_statement(phrase1, phrase2):
-    return phrase1 + phrase2
+    return phrase1 + "   " + phrase2
 
 def random_statement(phraselist):
     statement = phraselist[random.randrange(len(phraselist))]
